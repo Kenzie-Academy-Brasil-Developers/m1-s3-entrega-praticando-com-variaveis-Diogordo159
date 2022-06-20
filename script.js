3bd1e9cdffdb0e9372465df33d529b2b40108446
Diogordo159 <106835287+Diogordo159@users.noreply.github.com>
2022-06-20 14:41:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome = "Jhon";
+let sobrenome = "Doyle";
+let anoNascimento = 1991;
+let anoAtual = 2022;
+let cidade = "Curitiba";
+
+console.log(`Eu sou o ${nome} ${sobrenome} tenho ${anoAtual -anoNascimento} anos e moro em ${cidade}`)
